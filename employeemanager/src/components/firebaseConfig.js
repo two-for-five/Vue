8e33b80f3f8dcc75259b0b1fe71dea8334c26445
Yifeng Liu <yifeng.liu@duke.edu>
2018-02-25 13:01:44 -0500

    Employee Manager App Done

---
+++ employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyCJY6urPZFTaVS9aap-csR8RR7_7BYtrYc",
+    authDomain: "vues-prod.firebaseapp.com",
+    databaseURL: "https://vues-prod.firebaseio.com",
+    projectId: "vues-prod",
+    storageBucket: "vues-prod.appspot.com",
+    messagingSenderId: "261189895154"
+};
